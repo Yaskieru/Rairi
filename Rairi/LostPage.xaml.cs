cabc28277c16fd5bd90388529273fa8d783f6fb2
Yaskieru <yalmario@mls.ceu.edu.ph>
2019-07-22 02:01:58 +0800

    * .gitignore: Initial push for Rairi App
    * Rairi.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * Rairi.csproj:
    * MainPage.xaml:
    * LostPage.xaml:
    * HomePage.xaml:
    * Info.plist:
    * FoundPage.xaml:
    * AccountPage.xaml:
    * HomePage.xaml.cs:
    * LostPage.xaml.cs:
    * MainPage.xaml.cs:
    * FoundPage.xaml.cs:
    * AppDelegate.cs:
    * AccountPage.xaml.cs:
    * Rairi.iOS.csproj:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * Default@2x.png:
    * Rairi.Android.csproj:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * styles.xml:
    * colors.xml:
    * file_path.xml:
    * AboutResources.txt:
    * Tabbar.axml:
    * LaunchScreen.storyboard:
    * Toolbar.axml:
    * Default-Portrait@2x.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon76.png:
    * Icon80.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon180.png:
    * Icon167.png:
    * Icon152.png:
    * Icon120.png:
    * icon_round.xml:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ Rairi/LostPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Rairi
+{
+    public partial class LostPage : ContentPage
+    {
+        public LostPage()
+        {
+            InitializeComponent();
+        }
+
+        void UploadButton_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Rairi/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Rairi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void HomeButton_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new HomePage());
+        }
+    }
+}
