61d1c8e3dc641580b6b72082ef7513b5af22c9a9
Yaskieru <yalmario@mls.ceu.edu.ph>
2019-07-23 01:22:00 +0800

    Added photo function

--- Rairi/LostPage.xaml.cs
+++ Rairi/LostPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Xamarin.Forms;
+using DeepAI;
+using System.IO;
 
 namespace Rairi
 {
@@ -12,9 +15,68 @@ namespace Rairi
             InitializeComponent();
         }
 
-        void UploadButton_Clicked(object sender, System.EventArgs e)
+        private async void UploadButton_Clicked(object sender, System.EventArgs e)
         {
-            throw new NotImplementedException();
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "Your device does not currently support this functionality", "Ok");
+                return;
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if (selectedImage == null)
+            {
+                await DisplayAlert("Error", "Could not get the image, please try again.", "Ok");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+
+        private async void UploadButton2_Clicked(object sender, System.EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "Your device does not currently support this functionality", "Ok");
+                return;
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if (selectedImage2 == null)
+            {
+                await DisplayAlert("Error", "Could not get the image, please try again.", "Ok");
+                return;
+            }
+
+            selectedImage2.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+
+        private async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DeepAI_API api = new DeepAI_API("cff6094b-1163-47f0-96c8-96965a790c36");
+            StandardApiResponse resp = api.callStandardApi("image-similarity", new
+            {
+                image1 = File.OpenRead(selectedImage2.Source.ToString()),
+                image2 = File.OpenRead("C:\\path\\to\\your\\file.jpg"),
+            });
+
+            await DisplayAlert("Message",api.objectAsJsonString(resp),"Ok");
         }
     }
 }
